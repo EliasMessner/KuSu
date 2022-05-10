e9478c260faa4528183fc4f9d9db16d70bf3afea
kf47xype <104018950+kf47xype@users.noreply.github.com>
2022-05-10 18:51:01 +0200

    Add files via upload
    Improved efficiency dramatically.

---
+++ atomize.py
@@ -0,0 +1,36 @@
+import sys
+
+def main():
+    sourcepath = "combined_data.xml"                    # where the entire XML data is now combined in one file
+
+    with open(sourcepath, "r") as f:                    # read the whole source
+        sourcedata = f.read()
+
+    with open("atomize.log", "w") as l:                 # set up a logging file
+        l.write("File, Size, Lines\n")
+
+    print("There are {} entries.".format(sourcedata.count("<lido:lido>")))      # a preview of how many output files there will be
+
+    counter = 0                                                                 # counter each document
+    posA = 0
+    posB = 0
+    while sourcedata.find("<lido:lido>", posB) != -1:                           # as long as there's another opening tag
+        posA = sourcedata.find("<lido:lido>", posB)                             # start of opening tag
+        posB = sourcedata.find("</lido:lido>", posA)+12                         # end of closing tag
+
+        entrydata = sourcedata[posA:posB]                                       # the part to be put in its own file
+        destinationPath = "./docs/atomized_{}.xml".format(counter)               # the file path to write it into
+
+        with open(destinationPath, "w") as g:                                   # write this entry
+            g.write(entrydata)
+
+        with open("atomize.log", "a") as l:
+            logdata = "{}, {}, {}\n".format(counter, len(entrydata), entrydata.count("\n"))
+            l.write(logdata)
+
+        counter += 1
+        sys.stdout.write("\r\tWrote {}".format(counter))
+
+
+if __name__ == "__main__":
+    main()
--- combine.py
+++ combine.py
@@ -1,23 +1,41 @@
+# The XML data we get from our source is split in 3 files, each 58 - 73 MiB large.
+# There are over 14.000 individual entries/documents in those files combined.
+# We only perform searches on these huge strings, no editing of any kind. As editing
+# a total of 197 MB of string each time we find an entry would consume enormous
+# amounts of processing power on weak hardware. For that, each search on that string
+# needs to be narrowed down. We search for closing tags only after the last opening tag,
+# and we only search for opening tags after the last closing tag. Without this simple step it
+# would only run at < 100 kiB/s, with it it processes the whole 197 MB in seconds.
+
+
 import sys
+
 
 def main():
     filepaths = ["mkg_lido-dC.web_0.xml", "mkg_lido-dC.web_1.xml", "mkg_lido-dC.web_2.xml"]         # files to be combined into one
     destination = "combined_data.xml"                                                               # destination
 
-    for item in filepaths:                                  # go through each file
+    for item in filepaths:                                          # go through each file
         with open(item, "r") as f:
-            data = f.read()                                 # get complete content of each file
-            print("Read {}".format(item))
+            data = f.read()                                         # get complete content of each file
 
-        with open(destination, "a") as g:                   # append to the destination file
-            while data.find("<lido:lido>") != -1:           # go on as long as there's another entry to be added
-                posA = data.find("<lido:lido>")             # find the start of the opening tag of 1 entry
-                posB = data.find("</lido:lido>")+12         # find the end of the closing tag of that entry
-                g.write(data[posA:posB] + "\n\n")           # add that entry to the destination file
-                data = data[posB:]                          # remove that entry from the data
-                remaining = round(len(data) / 2**20, 3)                         # how much data is left in this file in MB
-                sys.stdout.write("\r{} MB remaining.".format(remaining))        # overwrite in the same line to avoid flooding the terminal
-        print("\nWrote that part.")
+        filesize = round(len(data)/2**20, 3)                        # filesize in MiB
+        print("Read {}\nSize: {} MB".format(item, filesize))
+
+        processed = 0                                               # processed data in MiB
+        posA = 0
+        posB = 0
+        with open(destination, "a") as g:
+            while data.find("<lido:lido>", posB) != -1:             # keep appending as long as there's another opening tag
+                posA = data.find("<lido:lido>", posB)               # find the opening tag, after the last closing tag
+                posB = data.find("</lido:lido>", posA)+12           # find the end of the closing tag, search after the last opening tag
+
+                entrydata = data[posA:posB]                         # the entry itself
+                g.write(entrydata + "\n\n")                         # add that entry to the destination file
+
+                processed += len(entrydata) / 2**20                                 # how much data is left in this file in MiB
+                sys.stdout.write("\r\t{} MB done.".format(round(processed,3)))      # overwrite in the same line to avoid flooding the terminal
+            print("\nFile done.\n")
 
 if __name__ == "__main__":
     main()
