beb676f9cf548489bb89e3b1fdbcdc93c6a93fc4
kf47xype <104018950+kf47xype@users.noreply.github.com>
2022-06-28 13:14:25 +0200

    Refactoring in constants.py
    Transformed file into a module to allow import, added variables which will be accessed.

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,20 @@
-urls = ["https://raw.githubusercontent.com/MKGHamburg/MKGCollectionOnlineLIDO_XML/master/mkg_lido-dC.web_0.xml",
-        "https://raw.githubusercontent.com/MKGHamburg/MKGCollectionOnlineLIDO_XML/master/mkg_lido-dC.web_1.xml",
-        "https://raw.githubusercontent.com/MKGHamburg/MKGCollectionOnlineLIDO_XML/master/mkg_lido-dC.web_2.xml"]
-
-data_dir = "../data/"
-
-logs_dir = "../logs/"
-
-docs_dir = "../docs/"
+class consants():
+    urls = ["https://raw.githubusercontent.com/MKGHamburg/MKGCollectionOnlineLIDO_XML/master/mkg_lido-dC.web_0.xml",
+            "https://raw.githubusercontent.com/MKGHamburg/MKGCollectionOnlineLIDO_XML/master/mkg_lido-dC.web_1.xml",
+            "https://raw.githubusercontent.com/MKGHamburg/MKGCollectionOnlineLIDO_XML/master/mkg_lido-dC.web_2.xml"]
+    data_dir = "../data/"
+    logs_dir = "../logs/"
+    docs_dir = "../docs/"
+    image_filepaths = "/Users/leagleissner/Documents/GitHub/InformationRetrieval/images"
+    image_ex_image_blue = "/Users/leagleissner/Documents/GitHub/InformationRetrieval/images/lamps/HM-11-223.JPG"
+    image_ex_image_rosa = "/Users/leagleissner/Documents/GitHub/InformationRetrieval/images/lamps/HM-11-207.JPG"
+    image_alle_lampen = "/Users/leagleissner/Documents/GitHub/InformationRetrieval/images/lamps"
+    image_saarland = "/Users/leagleissner/Documents/GitHub/InformationRetrieval/images/saarland/"
+    scale_percent = 5
+    n_clusters = 5
+    pbar_update_n = 1
+    plt_xticks_rotation = 'vertical'
+    
+    
+if __name__ == "__main__":
+    c = consants()
