9048cc54016b38f8def6439bd2c4739ea9f2b8ab
kf47xype <104018950+kf47xype@users.noreply.github.com>
2022-07-05 14:27:11 +0200

    Functional Website
    Added a transparent image as background. Added source attribution at the bottom. Removed obsolete code.

--- src/server.py
+++ src/server.py
@@ -3,23 +3,37 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():                                                                # the default state when opened
-    return render_template("index.html")                                    # wird von Flask relativ zu diesem file in "./templates/" gesucht
+def index():                                                    # the default state when opened
+    return render_template("index.html")                        # wird von Flask relativ zu diesem file in "./templates/" gesucht
 
 
-@app.route("/", methods=["POST","GET"])                                     # what happens if the button is pressed
+@app.route("/", methods=["POST","GET"])                         # what happens if the button is pressed
 def form_post():
     if request.method == "POST":
-        query = request.form["query"]
-        years = [request.form["minyear"], request.form["maxyear"]]
-        colors = request.form["colors"].split(",")
+        query = request.form["query"]                           # the written user input
+        colors = request.form["colors"]                         # 0 to all colors as a string separated by comma
+        if colors.find(",") != -1:
+            colors = colors.split(",")
+        categories = request.form["categories"]                 # 0 to all categories as a string separated by comma
+        if categories.find(",") != -1:
+            categories = categories.split(",")
 
-        results = [ [query, minyear, colors] , [query, minyear, colors] ]         # placeholder
-                                                                            # insert everything here
-                                                                            # results need to be a list of lists as [title, author, url]
-                                                                            # necessary for a variable-sized table
+        """
+        call search engine with everything that belongs to it here.
+        I didn't work on the backend, don't know precisely how to.
+        Variables for search engine query, colors, and categories.
 
-    return render_template("index.html", query=query, results=results)  # updates the page
+        results = [ [title,author,url] , [title,author,url] , ... ]
+
+        if more columns are desired update the result table in the HTML file accordingly and add the variable here.
+
+        if search engine returns too many results:
+        results = results[:30]
+        or any other appropriate rank-based lower limit
+
+        """
+
+    return render_template("index.html", query=query, results=results)      # this updates the HTML page with the results
 
 
 if __name__ == '__main__':
