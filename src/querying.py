5cf146de421be1cbcfd134d35b14973f9f2f4ddf
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-07-13 20:36:29 +0200

    Combined and/or/match-phrase in querying.py Made lido handler parse lamps description

--- src/querying.py
+++ src/querying.py
@@ -16,5 +16,45 @@ def get_query_body(query_string: str) -> dict:
     }
 
 
+def get_improved_query_body(query_string: str) -> dict:
+    """
+    inserts given query string into elasticsearch query body so that results are ranked highest if they match the phrase
+    exactly. If they don't match exactly, they are still ranked in a more-matches-is-better approach.
+    :param query_string:
+    :return:
+    """
+    return {
+        "query": {
+            "bool": {
+                "should": [
+                    {
+                        "match": {
+                            "content": {
+                                "query": query_string
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "content": {
+                                "query": query_string,
+                                "operator": "and"
+                            }
+                        }
+                    },
+                    {
+                        "match_phrase": {
+                            "content": {
+                                "query": query_string,
+                                "boost": 2
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+
 def search(client: Elasticsearch, index: str, query_string: str, size=20):
-    return client.search(index=index, body=get_query_body(query_string), size=size)
+    return client.search(index=index, body=get_improved_query_body(query_string), size=size)
