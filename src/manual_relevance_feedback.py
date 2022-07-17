16602b92a96a749f64afa5c9d2f4ca0be19466ca
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-07-17 16:53:44 +0200

    Implemented gradual relevance feedback in manual_relevance_feedback.py

--- src/manual_relevance_feedback.py
+++ src/manual_relevance_feedback.py
@@ -21,11 +21,11 @@ def main():
             print(f"\n\nQuery {topic['number']}/{len(topics)}: {topic['query']}\n###########")
             hits = get_all_hits_from_all_configs_merged_as_set(topic["query"], client=client, size=20)
             if not len(hits):
-                print("No Results.")
+                print("No Results.\n")
             for i, hit in enumerate(hits, start=1):
                 print(f"\nResult {i}/{len(hits)}:")
                 print(prettify(hit))
-                rel = get_rel_feedback()
+                rel = get_rel_feedback(0, 5)
                 new_line = f"{topic['number']} 0 {hit['_id']} {rel}\n"
                 rel_feedback_file.write(new_line)
 
@@ -40,16 +40,31 @@ def ask_queries_filename():
     return queries_filename
 
 
-def get_rel_feedback():
-    msg = "relevant? [y/n]"
+def get_rel_feedback(min_value: int, max_value: int):
+    msg = f"How relevant? [{min_value}-{max_value}]"
     while True:
         rel = input(msg)
-        if rel == 'y':
-            return 1
-        if rel == 'n':
-            return 0
-        msg = f"Bad response '{rel}'. Enter 'y' or 'n'"
+        if rel.isdigit() and min_value <= int(rel) <= max_value:
+            return int(rel)
+        msg = f"Bad response '{rel}'. Enter number from {min_value} to {max_value}"
+
+
+def print_results_without_asking_feedback():
+    client = es_helper.prepare_client_dialog()
+    time.sleep(2)  # sleep so that elasticsearch warnings can be output and not interrupt the following outputs
+    queries_filename = ask_queries_filename()
+    topics = parse_topics(os.path.join(queries_dir, queries_filename))
+    for topic in topics:
+        print(f"\n\nQuery {topic['number']}/{len(topics)}: {topic['query']}\n###########")
+        hits = get_all_hits_from_all_configs_merged_as_set(topic["query"], client=client, size=20)
+        if not len(hits):
+            print("No Results.\n")
+        for i, hit in enumerate(hits, start=1):
+            print(f"\nResult {i}/{len(hits)}:")
+            print(prettify(hit))
+            print("How relevant? [0-5]")
 
 
 if __name__ == "__main__":
     main()
+    # print_results_without_asking_feedback()
